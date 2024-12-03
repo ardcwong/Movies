0d03463182085b64cbed0b43e9a6e19f1136bb73
ardcwong <168283178+ardcwong@users.noreply.github.com>
2024-12-03 22:57:04 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -91,7 +91,7 @@ with col2:
         return prediction
     
     if st.button("Predict"):
-        output = predict_if_AAA(X_holdout_id_map[X_holdout_id_map['Title'] == choice].index[0])
+        output = predict_if_AAA(X_holdout_id_map[X_holdout_id_map['primaryTitle'] == choice].index[0])
     
         if output == 'Fraud':
             st.sucess('AAA')
